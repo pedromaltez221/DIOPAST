5b03797bd68f93485c0c531e3f5e6c39e717ad2e
Joao Pedro <pedro.maltez2@gmail.com>
2022-07-07 20:30:15 -0300

    Jogo DA MEMORIA

---
+++ JogoDaMemoria/script.js
@@ -0,0 +1,69 @@
+const CARDS = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
+function flipCard(){
+
+    if(lockBoard){
+        return;
+    }
+
+    if(this === firstCard)return
+
+    this.classList.add('flip');
+
+    if(!hasFlippedCard){
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMath();
+}
+
+CARDS.forEach((card) => {
+    card.addEventListener('click',flipCard);
+});
+
+function checkForMath(){
+
+    if(firstCard.dataset.card === secondCard.dataset.card){
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+function disableCards(){
+
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click',flipCard);
+    resetBoard();
+}
+
+function unflipCards(){
+
+    lockBoard = true;
+    setTimeout(() =>{
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+    resetBoard();
+    },1500);
+}
+
+(function shuffler(){
+    CARDS.forEach((card)=>{
+       let randomPosition = Math.floor(Math.random()*12);
+       card.style.order = randomPosition;
+    })
+})();
+
+function resetBoard(){
+    [hasFlippedCard,lockBoard] = [false,false];
+    [firstCard,secondCard] = [null,null];
+}
+
