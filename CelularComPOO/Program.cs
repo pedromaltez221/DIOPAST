770ad27d6f4d11cec0b805b9d538aced6dc7d0af
Joao Pedro <pedro.maltez2@gmail.com>
2022-12-17 21:39:09 -0300

    POO SmartPhone

---
+++ CelularComPOO/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Nokia:");
+Nokia nokia = new Nokia("(31988097344)","Xiaomi","123123123",16);
+
+nokia.Ligar();
+nokia.InstalarAplicativo("WPP");
+
+Console.WriteLine("Apple");
+Iphone apple = new Iphone("(31988097345)","Apple","123123125",16);
+apple.Ligar();
+apple.InstalarAplicativo("TL");
+// TODO: Realizar os testes com as classes Nokia e Iphone
